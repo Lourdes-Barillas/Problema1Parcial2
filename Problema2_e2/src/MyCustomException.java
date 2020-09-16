e74922d848f42ef125804cf63b468e91b53ee541
Adriana Pérez <adriana.perez5335@gmail.com>
2020-09-16 12:57:18 -0600

    Problema2_e2

---
+++ Problema2_e2/src/MyCustomException.java
@@ -0,0 +1,10 @@
+public class MyCustomException extends Exception{
+    private Errores error;
+
+    public void validarCarnet(int codeSede, int codeAlumno){
+        //contar los dígitos del código de sede
+        if((Integer.toString(codeSede)).length()>4){
+            
+        }
+    }
+}

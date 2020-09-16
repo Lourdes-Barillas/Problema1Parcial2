2dd378a76a1dcf953b787b4aa758caa46c1fc37e
Adriana Pérez <adriana.perez5335@gmail.com>
2020-09-16 04:31:44 -0600

    Comienzo del problema1

---
+++ Problema1_Examen2/src/Vehiculo.java
@@ -0,0 +1,13 @@
+public class Vehiculo {
+    private String placa;
+    private String marca;
+    private String modelo;
+    private String color;
+
+    public Vehiculo(){
+        placa = "";
+        marca = "";
+        modelo = "";
+        color = "";
+    }
+}
